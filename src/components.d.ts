fe626d5f7932d1683a945314d88ae18a52edfb8f
TalexDreamSoul <59305952+TalexDreamSoul@users.noreply.github.com>
2024-02-01 14:46:06 +0800

    Update components.d.ts

--- src/components.d.ts
+++ src/components.d.ts
@@ -24,6 +24,7 @@ declare module 'vue' {
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElImage: typeof import('element-plus/es')['ElImage']
     ElInput: typeof import('element-plus/es')['ElInput']
+    ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElOptionGroup: typeof import('element-plus/es')['ElOptionGroup']
