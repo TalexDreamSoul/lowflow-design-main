89f9a8897c5a763ed35994386899d1a88a23b489
balle <1490681144e@qq.com>
2024-02-19 21:22:12 +0800

    策略器目标设置

--- src/touch-flow/p/start/SubDiversionAttr.vue
+++ src/touch-flow/p/start/SubDiversionAttr.vue
@@ -13,6 +13,67 @@ const origin = {
   nodeType: "subDiversion",
   nodeName: "test",
   value1: false,
+
+  targetRuleContent: {
+				targetDelayed: {
+					delayedAction: "",
+					delayedTime: 0,
+					delayedType: "",
+					delayedUnit: "day",
+					isDelayed: false
+				},
+				targetRuleContent: {
+					eventSequence: {
+						conditions: [
+							{
+								conditions: [
+									{
+										action: "",
+										conditions: {
+											conditions: [
+												{
+													attr: {
+														field: "",
+														fieldMultiValue: [],
+														fieldName: "",
+														fieldOp: "",
+														fieldRangeValue: "",
+														fieldType: "",
+														fieldValue: "",
+														timeCondition: {
+															endDay: 0,
+															endTime: "",
+															isFuture: false,
+															isWithin: false,
+															startDay: 0,
+															startTime: "",
+															timeType: ""
+														}
+													},
+													label: {
+														labelId: 0,
+														labelName: "",
+														labelValue: []
+													},
+													type: ""
+												}
+											],
+											logicalChar: ""
+										},
+										eventCode: "",
+										eventName: ""
+									}
+								],
+								endTime: "",
+								logicalChar: "",
+								startTime: ""
+							}
+						],
+						logicalChar: ""
+					},
+					logicalChar: ""
+				}
+			},
   isDelayed: false,
   selectedType: "day",
   delayedAction: "day",
@@ -394,23 +455,25 @@ const platformOptions: any = {
           </div>
           <div class="yugu_flex">
             <div class="title">策略器目标设置 &nbsp;</div>
-            <el-switch v-model="sizeForm.value1" />
+            <el-switch v-model="sizeForm.targetRuleContent.targetDelayed.isDelayed" />
           </div>
+
+          <div v-if="sizeForm.targetRuleContent.targetDelayed.isDelayed">
+            <div class="garyblock">
+              <el-text>该策略器的延时以及动作执行完毕后，在</el-text>&nbsp;
+              <el-input v-model="sizeForm.targetRuleContent.targetDelayed.delayedTime" type="number" style="width: 100px" />&nbsp;
+              <el-select v-model="sizeForm.targetRuleContent.targetDelayed.delayedUnit" style="width: 150px">
+                <el-option value="month" label="月份">分钟</el-option>
+                <el-option value="week" label="周">小时</el-option>
+                <el-option value="day" label="天">天</el-option> </el-select>&nbsp;
+              <el-text>后立即判断客户是否完成以下转化事件，则认为完成该策略器模板。</el-text>
+            </div>
+            <EventBehavior :custom="sizeForm.eventRuleContent!.customEvent" />
+          </div>
+
         </div>
       </div>
 
-      <el-form-item label="策略器目标设置">
-        <el-switch v-model="sizeForm.target" />
-      </el-form-item>
-
-      <div v-if="sizeForm.target">
-        该策略器的延时以及动作执行完毕后，在
-        <el-input />
-        <el-input />
-        后立即判断客户是否完成以下转化事件，则认为完成该策略器模板。
-
-        <EventBehavior :custom="sizeForm.eventRuleContent!.customEvent" />
-      </div>
     </el-form>
   </div>
 </template>
