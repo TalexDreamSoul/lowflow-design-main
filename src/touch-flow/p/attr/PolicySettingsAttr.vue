872b0693fef9bbc2d33af639bc348b1d0243d891
TalexDreamSoul <59305952+TalexDreamSoul@users.noreply.github.com>
2024-03-02 13:09:57 +0800

    ref: 选择策略器预估统计

--- src/touch-flow/p/attr/PolicySettingsAttr.vue
+++ src/touch-flow/p/attr/PolicySettingsAttr.vue
@@ -2,7 +2,6 @@
 import { inject, ref, reactive, watchEffect } from "vue";
 import { ElMessage } from "element-plus";
 import { randomStr } from "~/utils/common";
-import { getmarketingTouchEstimate } from "~/api";
 import BehaviorGroup from "../behavior/BehaviorGroup.vue";
 import CustomAttr from "../behavior/CustomAttr.vue";
 import CustomBehavior from "../behavior/CustomBehavior.vue";
@@ -10,11 +9,11 @@ import CustomBehaviorSequence from "../behavior/sequence/CustomBehaviorSequence.
 import LogicalLine from "../behavior/LogicalLine.vue";
 import BehaviorGroupPlus from "../behavior/BehaviorGroupPlus.vue";
 import EventBehavior from "../behavior/EventBehavior.vue";
+import TouchEstimation from "~/touch-flow/page/TouchEstimation.vue";
 import { markRaw } from "vue";
 import CommonAttr from "../start/CommonAttr.vue";
-import StrategistTargetAttr from "~/touch-flow/page/StrategistTargetAttr.vue";
 import { validateCommonDays } from "~/touch-flow/flow-utils";
-import { MarketingTouchNodeEditDTO } from "~/touch-flow/touch-total";
+import { CustomSearchDTO, MarketingTouchNodeEditDTO } from "~/touch-flow/touch-total";
 
 const origin: MarketingTouchNodeEditDTO = {
   nodeId: "",
@@ -68,13 +67,45 @@ const origin: MarketingTouchNodeEditDTO = {
   },
 };
 
-const marketingTouchNode = ref({
-  appPushCount: 0,
-  digitalCount: 0,
-  outboundCount: 0,
-  smsCount: 0,
-  total: 0,
-  znxCount: 0,
+const customRuleContent = reactive<CustomSearchDTO>({
+  customAttr: {
+    conditions: [
+      {
+        conditions: [
+          // {}
+        ],
+        logicalChar: "或",
+      },
+    ],
+    logicalChar: "或",
+  },
+  customEvent: {
+    conditions: [
+      {
+        conditions: [
+          // {}
+        ],
+        logicalChar: "或",
+      },
+    ],
+    logicalChar: "或",
+  },
+  eventSequence: {
+    conditions: [
+      {
+        conditions: [
+          // {}
+        ],
+        logicalChar: "或",
+      },
+    ],
+    logicalChar: "或",
+  },
+  logicalChar: "或",
+  blackList: {
+    _enable: "no",
+    data: [],
+  },
 });
 
 const props = defineProps<{
@@ -85,8 +116,6 @@ const props = defineProps<{
 
 const touchSettingsRef = ref();
 const sizeForm = reactive<typeof origin>(origin);
-
-console.log("here", sizeForm, props.p, props.new);
 
 watchEffect(() => {
   const { nodeType, nodeId } = props.p;
@@ -162,26 +191,6 @@ type IRegSaveFunc = (regFunc: () => boolean) => void;
 const regSaveFunc: IRegSaveFunc = inject("save")!;
 regSaveFunc(saveData);
 
-const estimation = async () => {
-  // 请求示例
-  let data = { ...sizeForm.customRuleContent };
-  let res = await getmarketingTouchEstimate(JSON.stringify(data));
-  res = {
-    data: {
-      total: 1000,
-      appPushCount: 500,
-      znxCount: 120,
-      digitalCount: 800,
-      outboundCount: 200,
-      smsCount: 499,
-    },
-    message: "交易成功",
-    code: "0",
-  };
-  marketingTouchNode.value = res.data;
-  console.log("Mounted", res);
-};
-
 function attrsAdd() {
   let attr: any = (sizeForm.customRuleContent!.customAttr!.conditions! =
     sizeForm.customRuleContent!.customAttr!.conditions! || []);
@@ -249,21 +258,19 @@ function sequenceAdd() {
       >
         <div class="titleCondition">进入该策略期的用户需要满足以下条件：</div>
         <el-form-item label="">
-          <div class="pannel">
-            <LogicalLine v-model="sizeForm.customRuleContent.logicalChar">
-              <BehaviorGroup @add="attrsAdd" title="客户属性满足">
-                <CustomAttr :custom="sizeForm.customRuleContent!.customAttr" />
-              </BehaviorGroup>
-              <BehaviorGroup @add="behaviorAdd" title="客户行为满足">
-                <CustomBehavior :custom="sizeForm.customRuleContent!.customEvent" />
-              </BehaviorGroup>
-              <BehaviorGroup @add="sequenceAdd" title="行为序列满足">
-                <CustomBehaviorSequence
-                  :custom="sizeForm.customRuleContent!.eventSequence"
-                />
-              </BehaviorGroup>
-            </LogicalLine>
-          </div>
+          <LogicalLine v-model="sizeForm.customRuleContent.logicalChar">
+            <BehaviorGroup @add="attrsAdd" title="客户属性满足">
+              <CustomAttr :custom="sizeForm.customRuleContent!.customAttr" />
+            </BehaviorGroup>
+            <BehaviorGroup @add="behaviorAdd" title="客户行为满足">
+              <CustomBehavior :custom="sizeForm.customRuleContent!.customEvent" />
+            </BehaviorGroup>
+            <BehaviorGroup @add="sequenceAdd" title="行为序列满足">
+              <CustomBehaviorSequence
+                :custom="sizeForm.customRuleContent!.eventSequence"
+              />
+            </BehaviorGroup>
+          </LogicalLine>
         </el-form-item>
       </BehaviorGroupPlus>
 
@@ -289,97 +296,15 @@ function sequenceAdd() {
           </div>
         </div>
         <el-form-item label="">
-          <div class="pannel">
-            <!-- <LogicalLine v-model="sizeForm.eventRuleContent.logicalChar"> -->
-            <EventBehavior :custom="sizeForm.eventRuleContent!.customEvent" />
-            <!-- </LogicalLine> -->
-          </div>
+          <!-- <LogicalLine v-model="sizeForm.eventRuleContent.logicalChar"> -->
+          <EventBehavior :custom="sizeForm.eventRuleContent!.customEvent" />
+          <!-- </LogicalLine> -->
         </el-form-item>
       </BehaviorGroupPlus>
 
       <CommonAttr ref="touchSettingsRef" :sizeForm="sizeForm" />
 
-      <div class="BlockBackground">
-        <div class="BlockBackground-Under">
-          <div class="yugu_flex">
-            <div class="title">预估触达客户 &nbsp;</div>
-            <el-button @click="estimation" class="buttonyugu" round>立即预估</el-button>
-          </div>
-          <div class="flexyugu">
-            <div class="grayblockfirst">
-              <div class="topName">预估受众客户总数</div>
-              <div v-if="marketingTouchNode.total != undefined">
-                {{ marketingTouchNode.total }}
-              </div>
-              <div style="color: #ff5050" v-else>无法预估数据</div>
-            </div>
-            <div class="grayblock">
-              <div class="innerblock">
-                <div>
-                  <div class="topName">APP Push</div>
-                  <div>
-                    {{
-                      marketingTouchNode.appPushCount != undefined
-                        ? marketingTouchNode.appPushCount
-                        : "-"
-                    }}
-                  </div>
-                </div>
-              </div>
-              <div class="innerblock">
-                <div>
-                  <div class="topName">APP内部</div>
-                  <div>
-                    {{
-                      marketingTouchNode.znxCount != undefined
-                        ? marketingTouchNode.znxCount
-                        : "-"
-                    }}
-                  </div>
-                </div>
-              </div>
-              <div class="innerblock">
-                <div>
-                  <div class="topName">企业微信</div>
-                  <div>
-                    {{
-                      marketingTouchNode.digitalCount != undefined
-                        ? marketingTouchNode.digitalCount
-                        : "-"
-                    }}
-                  </div>
-                </div>
-              </div>
-              <div class="innerblock">
-                <div>
-                  <div class="topName">智能外呼</div>
-                  <div>
-                    {{
-                      marketingTouchNode.outboundCount != undefined
-                        ? marketingTouchNode.outboundCount
-                        : "-"
-                    }}
-                  </div>
-                </div>
-              </div>
-              <div class="innerblock">
-                <div>
-                  <div class="topName">手机短信</div>
-                  <div>
-                    {{
-                      marketingTouchNode.smsCount != undefined
-                        ? marketingTouchNode.smsCount
-                        : "-"
-                    }}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <StrategistTargetAttr :readonly="readonly" :size-form="sizeForm" />
-      </div>
+      <TouchEstimation :readonly="readonly" :custom-rule-content="customRuleContent" />
     </el-form>
   </div>
 </template>
@@ -441,11 +366,6 @@ li:has(.template-option):has(.template-desc) {
   //background-color: #f5f8fc;
 }
 
-.underright {
-  width: 100%;
-  height: 12px;
-}
-
 .filter-filter-item__add {
   position: absolute;
   right: 12px;
@@ -455,16 +375,6 @@ li:has(.template-option):has(.template-desc) {
   display: -webkit-inline-box !important;
   align-items: flex-start !important;
   flex-direction: column !important;
-}
-
-.pannel {
-  width: 100%;
-  min-height: 200px;
-}
-
-.underright {
-  width: 100%;
-  height: 12px;
 }
 
 :deep(.el-form-item) {
@@ -500,77 +410,6 @@ li:has(.template-option):has(.template-desc) {
   .BlockBackground-Under {
     padding: 12px;
     background: #f7f8fa;
-  }
-}
-
-.yugu_flex {
-  display: flex;
-  align-items: center;
-  min-height: 48px;
-  margin-bottom: 8px;
-
-  .title {
-    margin-left: 8px;
-    font-size: 14px;
-    font-weight: 500;
-    color: rgba(0, 0, 0, 0.9);
-  }
-
-  .buttonyugu {
-    background: linear-gradient(rgb(32, 92, 203) 0%, rgb(89, 143, 241) 100%);
-    margin-left: 12px;
-    color: #ffffff;
-    height: 32px;
-  }
-}
-
-.flexyugu {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  margin-bottom: 12px;
-}
-
-.grayblock {
-  //width: 120px;
-  background: #ffffff;
-  border-radius: 4px 4px 4px 4px;
-  //margin-right: 12px;
-  display: flex;
-  align-items: center;
-  min-height: 48px;
-
-  .innerblock {
-    border-right: 1px solid rgba(0, 0, 0, 0.1);
-    padding: 20px;
-  }
-
-  .innerblock:last-child {
-    /* 样式属性 */
-    border-right: none;
-  }
-
-  .topName {
-    font-size: 14px;
-    font-weight: 400;
-    color: rgba(0, 0, 0, 0.6);
-    margin-bottom: 8px;
-  }
-}
-
-.grayblockfirst {
-  //width: 160px;
-  /* 其他样式属性可以根据需求添加 */
-  margin-right: 12px;
-  background: #ffffff;
-  border-radius: 4px 4px 4px 4px;
-  padding: 20px;
-
-  .topName {
-    font-size: 14px;
-    font-weight: 400;
-    color: rgba(0, 0, 0, 0.6);
-    margin-bottom: 8px;
   }
 }
 </style>
