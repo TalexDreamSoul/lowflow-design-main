15299beb70cd25aee4ccf666020d98a85de0c88d
TalexDreamSoul <59305952+TalexDreamSoul@users.noreply.github.com>
2024-03-05 19:32:16 +0800

    修复数据结构问题

--- src/touch-flow/p/attr/ConditionSetAttr.vue
+++ src/touch-flow/p/attr/ConditionSetAttr.vue
@@ -148,7 +148,7 @@ function saveData() {
   // 判断流程开始时间
   const { repeatTime } = sizeForm;
   console.log("rt", repeatTime);
-  if (lp !== "immediately" && !validatePropValue(repeatTime)) {
+  if (lp == "repeat" && !validatePropValue(repeatTime)) {
     ElMessage({
       message: "请正确填写流程开始时间！",
       type: "error",
@@ -174,6 +174,7 @@ function saveData() {
     enterType,
     enterCount,
     enterDay,
+    triggerRuleContent: sizeForm.triggerRuleContent
   });
 
   return true;
