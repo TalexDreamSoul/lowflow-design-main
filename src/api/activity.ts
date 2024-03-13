c18687d9f2c683c6205bec4b0124839893e56e93
郝乐 <v-haole@4paradigm.com>
2024-03-13 15:10:19 +0800

    删除模板

--- src/api/activity.ts
+++ src/api/activity.ts
@@ -7,7 +7,23 @@ export const pageActivityList = (data: any)=> {
 export const saveActivityTemplate = (data:any)=> {
   return request.post({ url: "/api/saveActivityTemplate.do", data });
 }
+interface ActivityTemplate {  
+  id: number;  
+  name: string;  
+  description: string;  
+  // 可以添加其他属性...  
+} 
 // 删除活动
 export const deleteActivityInfo = (data:any)=> {
   return request.post({ url: "/api/deleteActivityInfo.do", data });
+}
+
+// 查询模板列表
+export const listActivityTemplate = ():Promise<any>=> {
+  return request.post({ url: "/api/listActivityTemplate.do" });
+}
+
+// 查询ip host
+export const queryDict = ():Promise<any>=> {
+  return request.get({ url: "/api/queryDict.do",params:{ dictKey: 'frontEndDomain'} });
 }
