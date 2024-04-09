ca05d4d69cef6135bc56c4877d54a5cd98fae5ad
balle <1490681144e@qq.com>
2024-04-09 15:55:16 +0800

    水印范围扩大

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <el-config-provider :button="{ autoInsertSpace: true }" :locale="zhCn">
-    <div class="common-layout">
-      <TopMenu v-if="!meta.hideTopMenu" />
-      <div class="content">
-        <el-watermark style="height: 100%;" :content='`${appOptions.user?.accountName}+${dayjs(new Date().getTime()).format("YYYY-MM-DD")}`' :font="{ color: 'rgba(0, 0, 0, 0.15)' }" v-if="!meta.hideTopMenu">
+    <el-watermark style="height: 100%;" :content='`${appOptions?.user?.accountName}+${dayjs(new Date().getTime()).format("YYYY-MM-DD")}`' :font="{ color: 'rgba(0, 0, 0, 0.15)' }">
+      <div class="common-layout">
+        <TopMenu v-if="!meta.hideTopMenu" />
+        <div class="content">
           <router-view></router-view>
-        </el-watermark>
-        <router-view v-if="meta.hideTopMenu"></router-view>
-
+        </div>
       </div>
-    </div>
+    </el-watermark>
+
   </el-config-provider>
 </template>
 
@@ -18,18 +17,17 @@
 import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 import TopMenu from "~/views/TopMenu/index.vue";
 import { computed, reactive, provide, watchEffect, ref, onMounted } from "vue";
-import {  onBeforeRouteUpdate,useRoute, useRouter } from "vue-router";
+import { onBeforeRouteUpdate, useRoute, useRouter } from "vue-router";
 import { useLocalStorage } from "@vueuse/core";
 import customerAPI from "~/api/account";
 import dayjs from "dayjs";
 
-
 onBeforeRouteUpdate((to, from, next) => {
-  if (to.path === '/') {
-    debugger
-    next('/dashboard'); // 这里可以根据实际需求重定向到其他路径
+  if (to.path === "/") {
+    debugger;
+    next("/dashboard"); // 这里可以根据实际需求重定向到其他路径
   } else {
-    debugger
+    debugger;
     next(); // 继续路由导航
   }
 });
@@ -45,23 +43,25 @@ const fetchDataApi = async () => {
 };
 
 watchEffect(() => {
-  $ignored: appOptions
+  $ignored: appOptions;
 
-  const { user }: any = appOptions.value
+  const { user }: any = appOptions.value;
   // console.log("user", user)
   if (!user?.accountName?.length) {
-  fetchDataApi();
+    fetchDataApi();
   } else {
-    const { id } = user
+    const { id } = user;
 
     setTimeout(async () => {
-      const res = await customerAPI.accountContainMenuList({ id, accountId: id })
+      const res = await customerAPI.accountContainMenuList({
+        id,
+        accountId: id,
+      });
 
       appOptions.value.menu = res?.data;
-    })
+    });
   }
-
-})
+});
 provide("appOptions", appOptions);
 </script>
 <style lang="scss">
