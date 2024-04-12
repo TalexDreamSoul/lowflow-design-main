03e6652313e06aa5c1b3c35d999d2ef039c7401f
balle <1490681144e@qq.com>
2024-04-12 16:50:26 +0800

    import 'element-plus/theme-chalk/src/message.scss'

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import "@antv/x6-vue-shape";
 
 import '~/utils/mention/mention'
-
+import 'element-plus/theme-chalk/src/message.scss'
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
