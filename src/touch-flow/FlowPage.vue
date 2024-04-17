0a8548f52ae92b80632cc0f70839b7d6ac1e8383
balle <1490681144e@qq.com>
2024-04-17 16:24:09 +0800

    编辑id的逻辑调整

--- src/touch-flow/FlowPage.vue
+++ src/touch-flow/FlowPage.vue
@@ -43,7 +43,7 @@ const flowOptions = reactive<
     },
   },
   p: {
-    nodeId: "root",/* randomStr(12) */
+    nodeId: "root" /* randomStr(12) */,
     nodeType: "Start",
     height: 250,
     children: [] as any[],
@@ -135,39 +135,38 @@ function transformNodes(__nodes: Array<any>) {
   const res: Array<any> = [];
 
   [...__nodes].forEach((_node: any) => {
-    const node = { ..._node }
-    const nodeFather = window.$getNodeById(node.preNodeId)
+    const node = { ..._node };
+    const nodeFather = window.$getNodeById(node.preNodeId);
 
     if (node.touchTemplateContent?.type) {
-      const type = node.touchTemplateContent.type
+      const type = node.touchTemplateContent.type;
 
-      if (type === 'sms') {
-        delete node.touchTemplateContent.appPushTemplate
-        delete node.touchTemplateContent.digitalTemplate
-        delete node.touchTemplateContent.outboundTemplate
-        delete node.touchTemplateContent.znxTemplate
-      } else if (type === 'znx') {
-        delete node.touchTemplateContent.appPushTemplate
-        delete node.touchTemplateContent.digitalTemplate
-        delete node.touchTemplateContent.outboundTemplate
-        delete node.touchTemplateContent.smsTemplate
-      } else if (type === 'appPush') {
-        delete node.touchTemplateContent.znxTemplate
-        delete node.touchTemplateContent.digitalTemplate
-        delete node.touchTemplateContent.outboundTemplate
-        delete node.touchTemplateContent.smsTemplate
-      } else if (type === 'outbound') {
-        delete node.touchTemplateContent.appPushTemplate
-        delete node.touchTemplateContent.digitalTemplate
-        delete node.touchTemplateContent.znxTemplate
-        delete node.touchTemplateContent.smsTemplate
-      } else if (type === 'digital') {
-        delete node.touchTemplateContent.appPushTemplate
-        delete node.touchTemplateContent.outboundTemplate
-        delete node.touchTemplateContent.znxTemplate
-        delete node.touchTemplateContent.smsTemplate
+      if (type === "sms") {
+        delete node.touchTemplateContent.appPushTemplate;
+        delete node.touchTemplateContent.digitalTemplate;
+        delete node.touchTemplateContent.outboundTemplate;
+        delete node.touchTemplateContent.znxTemplate;
+      } else if (type === "znx") {
+        delete node.touchTemplateContent.appPushTemplate;
+        delete node.touchTemplateContent.digitalTemplate;
+        delete node.touchTemplateContent.outboundTemplate;
+        delete node.touchTemplateContent.smsTemplate;
+      } else if (type === "appPush") {
+        delete node.touchTemplateContent.znxTemplate;
+        delete node.touchTemplateContent.digitalTemplate;
+        delete node.touchTemplateContent.outboundTemplate;
+        delete node.touchTemplateContent.smsTemplate;
+      } else if (type === "outbound") {
+        delete node.touchTemplateContent.appPushTemplate;
+        delete node.touchTemplateContent.digitalTemplate;
+        delete node.touchTemplateContent.znxTemplate;
+        delete node.touchTemplateContent.smsTemplate;
+      } else if (type === "digital") {
+        delete node.touchTemplateContent.appPushTemplate;
+        delete node.touchTemplateContent.outboundTemplate;
+        delete node.touchTemplateContent.znxTemplate;
+        delete node.touchTemplateContent.smsTemplate;
       }
-
     }
 
     node.$id && (node.id = node.$id);
@@ -235,10 +234,16 @@ function flatMaps(__nodes: Array<any>) {
     const node = stack.pop();
 
     const obj = { ...node };
-    console.log(obj," (obj.$id && (obj.id = obj.$id, delete obj.$id)), delete obj.id");
+    console.log(
+      obj,
+      " (obj.$id && (obj.id = obj.$id, delete obj.$id)), delete obj.id"
+    );
     // debugger
-    (obj.$id && (obj.id = obj.$id, delete obj.$id))
-  
+    obj.$id && ((obj.id = obj.$id), delete obj.$id);
+    // 判断obj.id是否为字符串类型
+    if (typeof obj.id === "string") {
+      obj.id = -1;
+    }
     if (obj.children) {
       stack.push(...node.children);
 
@@ -279,9 +284,12 @@ async function submitReview(status: string = "approvalPending") {
     status,
   };
   if (route.params?.id?.length) {
-    Object.assign(data, { ..._flowOptions, id: route.params?.id, status: status });
+    Object.assign(data, {
+      ..._flowOptions,
+      id: route.params?.id,
+      status: status,
+    });
   } else {
-
     Object.assign(_flowOptions, { ..._flowOptions, status: status });
     Object.assign(data, _flowOptions);
   }
@@ -298,7 +306,7 @@ async function submitReview(status: string = "approvalPending") {
   title.value = "提交完毕";
   content.value = res.message || "失败";
 
-  if (res?.code === '0') {
+  if (res?.code === "0") {
     router.push("/touchCenter/touchList");
   }
 
@@ -328,16 +336,14 @@ const goBack = () => {
 
 <template>
   <div class="FlowPage">
-    <el-container :class="{ shrink: modelValue, readonly, expand: flowOptions.basic._expand }"
-      class="FlowPage-Container">
+    <el-container :class="{ shrink: modelValue, readonly, expand: flowOptions.basic._expand }" class="FlowPage-Container">
       <el-header>
         <FlowHeader v-if="!modelValue || !readonly" :basic="flowOptions.basic">
           <template #controller>
             <div>
               <el-button @click="goBack" round>返回</el-button>
               <el-button @click="submitReview('draft')" round>保存草稿</el-button>
-              <el-button round type="primary" @click="submitReview('approvalPending')"
-                class="primaryStyle">提交审核</el-button>
+              <el-button round type="primary" @click="submitReview('approvalPending')" class="primaryStyle">提交审核</el-button>
             </div>
           </template>
         </FlowHeader>
@@ -360,8 +366,7 @@ const goBack = () => {
 
   <teleport to="body">
     <el-dialog title="流程基础设置" v-model="dialogVisible">
-      <FlowHeader :readonly="readonly" :expandAll="true" class="FlowPage-ShrinkHeader" @submit-review="submitReview"
-        :basic="flowOptions.basic" />
+      <FlowHeader :readonly="readonly" :expandAll="true" class="FlowPage-ShrinkHeader" @submit-review="submitReview" :basic="flowOptions.basic" />
     </el-dialog>
   </teleport>
 </template>
@@ -434,7 +439,7 @@ div.el-dialog {
     align-items: center;
 
     background-image: linear-gradient(rgba(0, 0, 0, 0.1) 1px, transparent 1px),
-    linear-gradient(90deg, rgba(0, 0, 0, 0.1) 1px, transparent 1px);
+      linear-gradient(90deg, rgba(0, 0, 0, 0.1) 1px, transparent 1px);
     background-size: 30px 30px;
 
     transform: translateY(80px);
