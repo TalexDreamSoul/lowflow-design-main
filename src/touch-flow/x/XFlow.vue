50062ee051d43452f898206c608c6662b6fd5875
balle <1490681144e@qq.com>
2024-04-17 14:12:03 +0800

    has报错解决

--- src/touch-flow/x/XFlow.vue
+++ src/touch-flow/x/XFlow.vue
@@ -141,7 +141,7 @@ const layoutFn = () => {
 
       if (data.diversionType || data.eventDelayed?.isDelayed) {
         calcHeight = calcHeight - 200;
-        if (data?.nodeContent?.data?.$template.has) {
+        if (data?.nodeContent?.data?.$template?.has) {
           calcHeight += 100;
         }
       }
