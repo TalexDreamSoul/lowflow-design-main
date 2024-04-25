da75c4987b7896369b7ac10d2f6b6dffe4f85ebd
balle <1490681144e@qq.com>
2024-04-25 17:09:33 +0800

    编辑时商品编号只读

--- src/views/channelManagement/equityManagement.vue
+++ src/views/channelManagement/equityManagement.vue
@@ -287,11 +287,10 @@ const onSubmit = async (formEl: FormInstance | undefined) => {
   </CustomEventComponent>
   <el-dialog class="pd-modal" destroy-on-close :close-on-click-modal="false" v-model="modalVisible" :title="ModalTitleMap[modalType]">
     <el-form :disabled="checkStringEqual(modalType, DrawerType.Detail)" ref="formRef" :hide-required-asterisk="true" label-position="top" class="form" :model="formValues">
-
-      <el-form-item :rules="[
+      <el-form-item  :rules="[
     { required: true, message: '请输入商品编号' }
   ]" label="商品编号" prop="skuCode">
-        <el-input v-model="formValues.skuCode" placeholder="请输入商品编号" clearable />
+        <el-input :disabled="modalType=='edit'" v-model="formValues.skuCode" placeholder="请输入商品编号" clearable />
       </el-form-item>
 
       <el-form-item :rules="[
