bd2cd4a69128b2a89ed96c56b343b6609c09e6e9
balle <1490681144e@qq.com>
2024-04-25 17:23:16 +0800

    交易对账调整

--- src/views/channelManagement/reconciliationList.vue
+++ src/views/channelManagement/reconciliationList.vue
@@ -145,7 +145,7 @@ function formatDate(value: number) {
     <template #search>
       <div class="search">
         <el-form :inline="true">
-          <el-form-item label="创建时间：">
+          <el-form-item label="交易日期：">
             <el-date-picker v-model="time" type="daterange" range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期" :size="size" @change="changeTime" />
           </el-form-item>
           <el-form-item>
